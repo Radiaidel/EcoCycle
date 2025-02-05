7a29fb64e79336f75d796fae4dd75dfa75ed64db
radia idelkadi <idelkadiradia@gmail.com>
2025-02-05 19:36:14 +0100

    Add design for Login, Register, and Dashboard pages

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,15 @@ module.exports = {
     "./src/**/*.{html,ts}",
   ],
   theme: {
-    extend: { },
+    extend: {
+      colors: {
+        'dark-green': '#0A1E28',
+        'green': {
+          600: '#4CAF50',
+          700: '#45A049'
+        }
+      }
+    }
   },
   plugins: [],
 }
